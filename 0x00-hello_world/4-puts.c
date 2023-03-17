5c661f626a100018dac28d67727b2c707dce5db2
nouman <noumanamrani9@gmail.com>
2023-03-16 17:34:14 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - puts
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-my readme file
+4. Hello, puts
