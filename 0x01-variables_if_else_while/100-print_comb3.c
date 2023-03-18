5c3f22158877911f3561d16960de4af1c49b6486
nouman <noumanamrani9@gmail.com>
2023-03-17 17:21:05 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,25 @@
  */
 int	main(void)
 {
-	int	a;
-	int	b;
+	int	i;
+	int	j;
 
-	a = 0;
-	while (a < 10)
+	i = 0;
+	while (i < 10)
 	{
-		b = a + 1;
-		while (b < 10)
+		j = i + 1;
+		while (j < 10)
 		{
-			putchar('0' + a);
-			putchar('0' + b);
-			if (i != 8 || b != 9)
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-			b++;
+			j++;
 		}
-		a++;
+		i++;
 	}
 	putchar('\n');
 	return (0);
