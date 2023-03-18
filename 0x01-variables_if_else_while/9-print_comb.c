73213ca5a0c226eed3dd9f64a2f86b34ab01f44b
nouman <noumanamrani9@gmail.com>
2023-03-17 17:09:25 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ * Return: 0 if the program finish whitout errors
+ */
+int	main(void)
+{
+	int	A;
+
+	A = '0';
+	while (A <= '9')
+	{
+		putchar(n);
+		if (A != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
