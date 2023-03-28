904d0bb37ad9bb2d8c31ad4fd6be88863c10e13d
nouman <noumanamrani9@gmail.com>
2023-03-28 09:32:43 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,48 @@
 #include "main.h"
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
+ * alx_putnbr - prints numbers < 1000
+ * @n: first arg'
+ * @m: second arg.
+ */
+void	alx_putnbr(int n, int m)
 {
-times_table();
-return (0);
+	if (n * m < 10)
+	{
+		if (m)
+			putchar(' ');
+		putchar('0' + n * m);
+	}
+	else
+	{
+		putchar('0' + (n * m) / 10);
+		putchar('0' + (n * m) % 10);
+	}
+}
+
+/**
+ * times_table - function that prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			alx_putnbr(i, j);
+			if (j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		putchar('\n');
+		i++;
+	}
 }
 
