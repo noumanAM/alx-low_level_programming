bafc836af402c595eac05069c932880c27c2342e
nouman <noumanamrani9@gmail.com>
2023-03-31 15:55:30 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,10 @@
 # include "main.h"
 
 /**
- * print_numbers - print 0 - 9
- *        only using _putchar twice 
- *
- * Returns: always 0 (success)        
+ * print_numbers - function that prints the numbers,
+ * from 0 to 9, followed by a new line.
  */
-
 void print_numbers(void)
 {
-              int num = 0;
-
-	      do {
-		      _putchar(num + 48);
-		      num++;
-	      } while (num >=0 && num <= 9);
-	         _putchar('\n');
+	write(1, "0123456789\n", 11);
 }
