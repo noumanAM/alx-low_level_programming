14c1f6f35da7bb687bfe9eba25c87531a274ef72
nouman <noumanamrani9@gmail.com>
2023-03-31 09:37:49 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,48 @@
+# include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: arg 1.
+ * Return: the length of a string.
+ */
+int _strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+/**
+ * swap_char - function that swaps the values of two integers.
+ * @a: arg 1.
+ * @b: arg 2.
+ */
+void swap_char(char *a, char *b)
+{
+	char	i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+}
+
+/**
+ * rev_string - function that reverses a string.
+ * @s: arg 1.
+ */
+void rev_string(char *s)
+{
+	int	i;
+	int	j;
+
+	i = _strlen(s) - 1;
+	j = 0;
+	while (j < _strlen(s) / 2)
+	{
+		swap_char(s + j, s + i);
+		j++;
+		i--;
+	}
+}
