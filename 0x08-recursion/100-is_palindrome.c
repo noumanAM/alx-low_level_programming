1e4f36a4ef3b49f3276358a43acb4ed308451e65
nouman <noumanamrani9@gmail.com>
2023-04-12 11:11:47 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,30 +1,29 @@
 #include "main.h"
 
 /**
- * is_prime_number2 -  function that returns 1 if the
- * input integer is a prime number, otherwise return 0.mber.
- * @x: Arg 1.
- * @y: Arg 1.
+ * is_palindrome2 - function that returns 1 if a string
+ * is a palindrome and 0 if not.
+ * @n: Arg 1.
+ * @x: Arg 2.
+ * @f: Arg 3.
  * Return: 1 or 0;
  */
-int is_prime_number2(int x, int y)
+int is_palindrome2(char *n, int x, int f)
 {
-	if (y > x / y)
-		return (1);
-	if (x % y == 0)
+	if (n[x] != n[f])
 		return (0);
-	return (is_prime_number2(x, y + 1));
+	if (x >= f)
+		return (1);
+	return (is_palindrome2(n, x + 1, f - 1));
 }
 
 /**
- * is_prime_number -  function that returns 1 if the
- * input integer is a prime number, otherwise return 0.mber.
- * @x: Arg 1.
- * Return: 1 or 0;
+ * is_palindrome - function that returns 1 if a string
+ * is a palindrome and 0 if not.
+ * @n: Arg 1.
+ * Return: 1 or 0.
  */
-int is_prime_number(int x)
+int is_palindrome(char *n)
 {
-	if (x < 0 || x == 1)
-		return (0);
-	return (is_prime_number2(x, 2));
+	return (is_palindrome2(n, 0, strlen(n) - 1));
 }
