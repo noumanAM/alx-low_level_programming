ca710af13964fcd5ffd2dc5a03339f0d73d1940d
nouman <noumanamrani9@gmail.com>
2023-04-12 11:12:55 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,27 +3,27 @@
 /**
  * is_palindrome2 - function that returns 1 if a string
  * is a palindrome and 0 if not.
- * @n: Arg 1.
- * @x: Arg 2.
- * @f: Arg 3.
+ * @s: Arg 1.
+ * @i: Arg 2.
+ * @y: Arg 3.
  * Return: 1 or 0;
  */
-int is_palindrome2(char *n, int x, int f)
+int is_palindrome2(char *s, int i, int y)
 {
-	if (n[x] != n[f])
+	if (s[i] != s[y])
 		return (0);
-	if (x >= f)
+	if (i >= y)
 		return (1);
-	return (is_palindrome2(n, x + 1, f - 1));
+	return (is_palindrome2(s, i + 1, y - 1));
 }
 
 /**
  * is_palindrome - function that returns 1 if a string
  * is a palindrome and 0 if not.
- * @n: Arg 1.
+ * @s: Arg 1.
  * Return: 1 or 0.
  */
-int is_palindrome(char *n)
+int is_palindrome(char *s)
 {
-	return (is_palindrome2(n, 0, strlen(n) - 1));
-}
+	return (is_palindrome2(s, 0, strlen(s) - 1));
+}}
