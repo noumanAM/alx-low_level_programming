a54b4040625cfb161ad37e3566b13516ae953a3a
nouman <noumanamrani9@gmail.com>
2023-04-13 15:48:55 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,13 +2,13 @@
 
 /**
  * *malloc_checked - allocates memory using malloc and exit if failed
- * @f: int
+ * @b: int
  * Return: pointer to the array initialized or null
  */
 
-void *malloc_checked(unsigned int f)
+void *malloc_checked(unsigned int b)
 {
-	int *m = malloc(f);
+	int *m = malloc(b);
 
 	if (m == 0)
 		exit(98);
