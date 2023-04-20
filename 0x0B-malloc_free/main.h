51fbd42f3b4ce15132af7e418f2b3bdf1689bb66
nouman <noumanamrani9@gmail.com>
2023-04-20 09:23:29 -0700

    task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,19 @@
-void free_grid(int **grid, int height);
-char *str_concat(char *s1, char *s2);
-char *_strdup(char *str);
-int _putchar(char c);
-char *create_array(unsigned int size, char c);
-int **alloc_grid(int width, int height);
-char *argstostr(int ac, char **av);
+#ifndef MAIN_H
+# define MAIN_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <time.h>
+# include <string.h>
+
+int		_putchar(char c);
+char	*create_array(unsigned int size, char c);
+char	*_strdup(char *str);
+int		**alloc_grid(int width, int height);
+char	*str_concat(char *s1, char *s2);
+void	free_grid(int **grid, int height);
+char	*argstostr(int ac, char **av);
+char	**strtow(char *str);
+
+#endif
